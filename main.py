a2b44c70d8268e7ba45448eb0f58f9a398866a9f
nxtdxve <zettlerd@bzz.ch>
2023-03-23 10:22:34 +0100

    simple main function

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import importlib
+# import get_current_data from data
+from data import get_current_data
 
-# Call the get_champion function to get the champion name
-champion_name = "Garen"
+def main():
+    get_current_data.main()
 
-try:
-    # Import the module for the given champion
-    champion_module = importlib.import_module(f"modules.{champion_name.lower()}")
-except ImportError:
-    print("Champion not supported.")
-
-# Call the desired function from the imported module
-level = 1
-champion_module.garen(level)
-champion_module.optimal_usage(2000, level)
+if __name__ == "__main__":
+    main()
